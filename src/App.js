1e2df68e1be0d938a0e6a21722beb03ea8905cc4
hossein.s <hsee1372@gmail.com>
2019-11-08 16:03:55 +0330

    mapAndItritevWarningUniqeKey

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      message:"my world getting little for this resents approach me "
+      message:"my world getting little for this resents approach me ",
+      monsters:[
+        {
+          name:"frankastain"
+         },
+         {
+           name:"Drucula"
+         },
+         {
+           name:"Zombi"
+         },
+         {
+           name:"Godzila"
+         },{
+           name:"Mother Of Dragon"
+         }
+      ]
     }
   }
 
@@ -16,6 +32,10 @@ class App extends Component {
     <h1>hi my world </h1>
     <h1>{this.state.message}</h1>
     <button onClick={()=>{this.setState({message:"Would you like to abolish this law"})}}>ClickMe</button>
+
+    <div>
+      {this.state.monsters.map(item=>(<h1>{item.name}</h1>))}
+    </div>
     </div>
   );
 }
