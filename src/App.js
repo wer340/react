a4c3691c1cc53e5b2cdad33ab16298d2de48f25a
hossein.s <hsee1372@gmail.com>
2019-11-08 15:53:16 +0330

    variableConsructorSuper

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      message:"my world getting little for this resents approach me "
+    }
+  }
+
   render(){
   return (
     <div className="App">
     <h1>hi my world </h1>
+    <h1>{this.state.message}</h1>
     </div>
   );
 }
