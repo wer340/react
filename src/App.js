cc1451588f039d36180b2376f2a076f2eb33ba7c
hossein.s <hsee1372@gmail.com>
2019-11-08 15:43:39 +0330

    class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>hi my name is hs</h1>
-        <a
-          className="App-link"npm run test
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>hi my world </h1>
     </div>
   );
 }
-
+}
 export default App;
