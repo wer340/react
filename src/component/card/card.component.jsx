53cf86429a57969b9ee6be1318739e541d9d0d50
hossein.s <hsee1372@gmail.com>
2019-11-09 12:14:55 +0330

    SearchFielinConsole

--- src/component/card/card.component.jsx
+++ src/component/card/card.component.jsx
@@ -7,7 +7,7 @@ import './card.styles.css'
 export const Card= props =>{
 return <div className='card-container'> 
  {/* { <img alt='flower' src={`http://lorempixel.com/400/200/fashion/${props.Monstering.id}/`}></img>} */}
-{ <img alt='flower' src={`https://loremflickr.com/180/180/celebrity/?lock=1${props.Monstering.id}`}></img>}
+{ <img alt='flower' src={`https://loremflickr.com/180/180/hollywood/?lock=1${props.Monstering.id}`}></img>}
 <h1>{props.Monstering.name}</h1>
 <h3>{props.Monstering.email}</h3>
 <h4>{props.Monstering.address.city}</h4>
