974c9f842be0e08797913e49a556c43a247b1367
hossein.s <hsee1372@gmail.com>
2019-11-09 20:17:56 +0330

    Updates

---
+++ precache-manifest.6c806382d4e5b778222727fb60a2a0e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687e735d46cbe466d3abc75dc294bcbb",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "ffdc581070ab4bbd15bc",
+    "url": "/reactjs/static/css/main.5fe5835c.chunk.css"
+  },
+  {
+    "revision": "decb733cc3e394fb7b4b",
+    "url": "/reactjs/static/js/2.9b1a50d6.chunk.js"
+  },
+  {
+    "revision": "ffdc581070ab4bbd15bc",
+    "url": "/reactjs/static/js/main.b039bb25.chunk.js"
+  },
+  {
+    "revision": "9e73a6123f394170771e",
+    "url": "/reactjs/static/js/runtime-main.93c41949.js"
+  }
+]);
